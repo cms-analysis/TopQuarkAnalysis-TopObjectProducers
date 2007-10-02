d5e65a3bb3fbcef0ea8ba84c0fa7dbb6959ef543
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-10-02 19:49:32 +0000

    Addition of filters requiring a minimum number of final state objects.

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,21 +1,21 @@
-
 #include "PluginManager/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_SEAL_MODULE();
 
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h"
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopMuonProducer.h"
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopTauProducer.h"
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopJetProducer.h"
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopMETProducer.h"
-#include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectSelector.h"
-
-DEFINE_SEAL_MODULE();
 
 DEFINE_ANOTHER_FWK_MODULE(TopElectronProducer);
-DEFINE_ANOTHER_FWK_MODULE(TopTauProducer);
 DEFINE_ANOTHER_FWK_MODULE(TopMuonProducer);
+DEFINE_ANOTHER_FWK_MODULE(TopTauProducer);
 DEFINE_ANOTHER_FWK_MODULE(TopJetProducer);
 DEFINE_ANOTHER_FWK_MODULE(TopMETProducer);
+
+#include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectSelector.h"
 
 DEFINE_ANOTHER_FWK_MODULE(CaloJetSelector);
 DEFINE_ANOTHER_FWK_MODULE(TopElectronSelector);
@@ -23,3 +23,13 @@ DEFINE_ANOTHER_FWK_MODULE(TopTauSelector);
 DEFINE_ANOTHER_FWK_MODULE(TopMuonSelector);
 DEFINE_ANOTHER_FWK_MODULE(TopJetSelector);
 DEFINE_ANOTHER_FWK_MODULE(TopMETSelector);
+DEFINE_ANOTHER_FWK_MODULE(TopParticleSelector);
+
+#include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectFilter.h"
+
+DEFINE_ANOTHER_FWK_MODULE(TopElectronCountFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopMuonCountFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopTauCountFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopJetCountFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopMETCountFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopParticleCountFilter);
