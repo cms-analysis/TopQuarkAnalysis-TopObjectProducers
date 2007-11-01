526ecd13d43409e43519e0c748c375b107e8d9dd
Steven Lowette <sha1-505c76c58f0059bdc87f89d448d5d320fbb25c2b@cern.ch>
2007-11-01 01:46:49 +0000

    Sync with TQAF_16X_2007: min-max filters and default cuts.

--- interface/TopObjectFilter.h
+++ interface/TopObjectFilter.h
@@ -2,13 +2,16 @@
 // Author:  Steven Lowette
 // Created: Tue Oct  2 19:55:00 CEST 2007
 //
-// $Id: TopObjectFilter.h,v 1.1 2007/10/02 19:21:31 lowette Exp $
+// $Id: TopObjectFilter.h,v 1.1.2.3 2007/11/01 01:11:32 lowette Exp $
 //
 
 #ifndef TopObjectProducer_TopObjectFilter_h
 #define TopObjectProducer_TopObjectFilter_h
 
 
+#include "PhysicsTools/UtilAlgos/interface/AnySelector.h"
+#include "PhysicsTools/UtilAlgos/interface/MinNumberSelector.h"
+#include "TopQuarkAnalysis/TopObjectProducers/interface/MaxNumberSelector.h"
 #include "PhysicsTools/UtilAlgos/interface/ObjectCountFilter.h"
 
 #include "AnalysisDataFormats/TopObjects/interface/TopElectron.h"
@@ -21,12 +24,19 @@
 #include <vector>
 
 
-typedef ObjectCountFilter<std::vector<TopElectron> > TopElectronCountFilter;
-typedef ObjectCountFilter<std::vector<TopMuon> >     TopMuonCountFilter;
-typedef ObjectCountFilter<std::vector<TopTau> >      TopTauCountFilter;
-typedef ObjectCountFilter<std::vector<TopJet> >      TopJetCountFilter;
-typedef ObjectCountFilter<std::vector<TopMET> >      TopMETCountFilter;
-typedef ObjectCountFilter<std::vector<TopParticle> > TopParticleCountFilter;
+typedef ObjectCountFilter<std::vector<TopElectron>, AnySelector<std::vector<TopElectron>::value_type>, MinNumberSelector> TopElectronMinFilter;
+typedef ObjectCountFilter<std::vector<TopMuon>,     AnySelector<std::vector<TopMuon>::value_type>,     MinNumberSelector> TopMuonMinFilter;
+typedef ObjectCountFilter<std::vector<TopTau>,      AnySelector<std::vector<TopTau>::value_type>,      MinNumberSelector> TopTauMinFilter;
+typedef ObjectCountFilter<std::vector<TopJet>,      AnySelector<std::vector<TopJet>::value_type>,      MinNumberSelector> TopJetMinFilter;
+typedef ObjectCountFilter<std::vector<TopMET>,      AnySelector<std::vector<TopMET>::value_type>,      MinNumberSelector> TopMETMinFilter;
+typedef ObjectCountFilter<std::vector<TopParticle>, AnySelector<std::vector<TopParticle>::value_type>, MinNumberSelector> TopParticleMinFilter;
+
+typedef ObjectCountFilter<std::vector<TopElectron>, AnySelector<std::vector<TopElectron>::value_type>, MaxNumberSelector> TopElectronMaxFilter;
+typedef ObjectCountFilter<std::vector<TopMuon>,     AnySelector<std::vector<TopMuon>::value_type>    , MaxNumberSelector> TopMuonMaxFilter;
+typedef ObjectCountFilter<std::vector<TopTau>,      AnySelector<std::vector<TopTau>::value_type>     , MaxNumberSelector> TopTauMaxFilter;
+typedef ObjectCountFilter<std::vector<TopJet>,      AnySelector<std::vector<TopJet>::value_type>     , MaxNumberSelector> TopJetMaxFilter;
+typedef ObjectCountFilter<std::vector<TopMET>,      AnySelector<std::vector<TopMET>::value_type>     , MaxNumberSelector> TopMETMaxFilter;
+typedef ObjectCountFilter<std::vector<TopParticle>, AnySelector<std::vector<TopParticle>::value_type>, MaxNumberSelector> TopParticleMaxFilter;
 
 
 #endif
--- src/SealModule.cc
+++ src/SealModule.cc
@@ -3,6 +3,7 @@
 
 DEFINE_SEAL_MODULE();
 
+
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopElectronProducer.h"
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopMuonProducer.h"
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopTauProducer.h"
@@ -15,6 +16,7 @@ DEFINE_ANOTHER_FWK_MODULE(TopTauProducer);
 DEFINE_ANOTHER_FWK_MODULE(TopJetProducer);
 DEFINE_ANOTHER_FWK_MODULE(TopMETProducer);
 
+
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectSelector.h"
 
 DEFINE_ANOTHER_FWK_MODULE(CaloJetSelector);
@@ -25,16 +27,27 @@ DEFINE_ANOTHER_FWK_MODULE(TopJetSelector);
 DEFINE_ANOTHER_FWK_MODULE(TopMETSelector);
 DEFINE_ANOTHER_FWK_MODULE(TopParticleSelector);
 
+
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopLeptonCountFilter.h"
-#include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectFilter.h"
 
 DEFINE_ANOTHER_FWK_MODULE(TopLeptonCountFilter);
-DEFINE_ANOTHER_FWK_MODULE(TopElectronCountFilter);
-DEFINE_ANOTHER_FWK_MODULE(TopMuonCountFilter);
-DEFINE_ANOTHER_FWK_MODULE(TopTauCountFilter);
-DEFINE_ANOTHER_FWK_MODULE(TopJetCountFilter);
-DEFINE_ANOTHER_FWK_MODULE(TopMETCountFilter);
-DEFINE_ANOTHER_FWK_MODULE(TopParticleCountFilter);
+
+#include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectFilter.h"
+
+DEFINE_ANOTHER_FWK_MODULE(TopElectronMinFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopMuonMinFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopTauMinFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopJetMinFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopMETMinFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopParticleMinFilter);
+
+DEFINE_ANOTHER_FWK_MODULE(TopElectronMaxFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopMuonMaxFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopTauMaxFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopJetMaxFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopMETMaxFilter);
+DEFINE_ANOTHER_FWK_MODULE(TopParticleMaxFilter);
+
 
 #include "TopQuarkAnalysis/TopObjectProducers/interface/TopObjectEnergyScale.h"
 
