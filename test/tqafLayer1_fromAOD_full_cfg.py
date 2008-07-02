10327c9605697ff157c716933691a6db21a92931
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-02 18:08:27 +0000

    cleanup and comments

--- test/tqafLayer1_fromAOD_full_cfg.py
+++ test/tqafLayer1_fromAOD_full_cfg.py
@@ -1,30 +1,73 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TQAF")
+#-------------------------------------------------
+# test cfg file for tqaflayer1 production from
+# fullsim
+#-------------------------------------------------
+process = cms.Process("TEST")
+
+## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
 
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
+#-------------------------------------------------
+# process configuration
+#-------------------------------------------------
 
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
-
-process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
-)
+## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTbar-210p5.1-AODSIM.100.root')
 )
 
+## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
+
+## configure process options
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+#-------------------------------------------------
+# tqaf configuration
+#-------------------------------------------------
+
+## std sequence for tqaf layer1
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
+
+#-------------------------------------------------
+# tqaf event content; first ALL objects are
+# dropped in this process; then tqafLayer1
+# concent is added
+#-------------------------------------------------
+
+## define event content
 process.tqafEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring('drop *')
 )
+
+## define tqaf layer1 event content
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
+process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
+
+
+#-------------------------------------------------
+# process output; first the event selection is
+# defined: only those events that have passed the
+# full production path are selected and written
+# to file; the event content has been defined
+# above
+#-------------------------------------------------
+
+## define event selection
 process.EventSelection = cms.PSet(
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('p')
     )
 )
+
+## configure output module
 process.out = cms.OutputModule("PoolOutputModule",
     process.EventSelection,
     process.tqafEventContent,
@@ -32,8 +75,14 @@ process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('TQAFLayer1_Output.fromAOD_full.root')
 )
 
-process.p = cms.Path(process.tqafLayer1)
-process.outpath = cms.EndPath(process.out)
-process.MessageLogger.cerr.threshold = 'INFO'
-process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
+#-------------------------------------------------
+# output paths; in order not to write the
+# persistent output to file comment the the output
+# path
+#-------------------------------------------------
 
+## process path
+process.p = cms.Path(process.tqafLayer1)
+
+## output
+process.outpath = cms.EndPath(process.out)
--- test/tqafLayer1_fromScratch_fast_cfg.py
+++ test/tqafLayer1_fromScratch_fast_cfg.py
@@ -1,28 +1,72 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TQAF")
-process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#-------------------------------------------------
+# test cfg file for tqaflayer1 production from
+# fastsim
+#-------------------------------------------------
+process = cms.Process("TEST")
 
+## add message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+
+#-------------------------------------------------
+# process configuration; be aware that here there
+# is no source; this is called within the fastsim
+# bootstrap
+#-------------------------------------------------
+
+## invoke fastsim bootstrap
 process.load("PhysicsTools.PatAlgos.famos.boostrapWithFamos_cff")
 
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_fast_cff")
-
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
-
-process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
-)
+## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(10)
 )
+
+## configure process options
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+#-------------------------------------------------
+# tqaf configuration
+#-------------------------------------------------
+
+## std sequence for tqaf layer1
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_fast_cff")
+
+#-------------------------------------------------
+# tqaf event content; first ALL objects are
+# dropped in this process; then tqafLayer1
+# concent is added
+#-------------------------------------------------
+
+## define event content
 process.tqafEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring('drop *')
 )
+
+## define tqaf layer1 event content
+process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
+process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
+
+#-------------------------------------------------
+# process output; first the event selection is
+# defined: only those events that have passed the
+# full production path are selected and written
+# to file; the event content has been defined
+# above
+#-------------------------------------------------
+
+## define event selection
 process.EventSelection = cms.PSet(
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('p')
     )
 )
+
+## configure output module
 process.out = cms.OutputModule("PoolOutputModule",
     process.EventSelection,
     process.tqafEventContent,
@@ -30,8 +74,14 @@ process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('TQAFLayer1_Output.fromScratch_fast.root')
 )
 
-process.p = cms.Path(process.famosWithEverythingPAT*process.tqafLayer1_withoutTrigMatch)
-process.outpath = cms.EndPath(process.out)
-process.MessageLogger.cerr.threshold = 'INFO'
-process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
+#-------------------------------------------------
+# output paths; in order not to write the
+# persistent output to file comment the the output
+# path
+#-------------------------------------------------
 
+## process path
+process.p = cms.Path(process.famosWithEverythingPAT*process.tqafLayer1_withoutTrigMatch)
+
+## output
+process.outpath = cms.EndPath(process.out)
