3e524dc174341abb28de78133e85de1668f03130
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-08-19 20:10:38 +0000

    extended tqaf event content

--- test/tqafLayer1_fromAOD_full_cfg.py
+++ test/tqafLayer1_fromAOD_full_cfg.py
@@ -33,7 +33,7 @@ process.source = cms.Source("PoolSource",
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1100)
+    input = cms.untracked.int32(1)
 )
 
 ## configure process options
@@ -59,6 +59,33 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 ## std sequence for tqaf layer1
 process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
 
+from PhysicsTools.PatAlgos.tools.jetTools import *
+
+## switch the jet collection
+## switchJetCollection(process, 
+##         'sisCone5CaloJets',    # jet collection; must be already in the event when patLayer0 sequence is executed
+##         layers=[0,1],          # if you're not running patLayer1, set 'layers=[0]' 
+##         runCleaner="CaloJet",  # =None if not to clean
+##         doJTA=True,            # run jet-track association & JetCharge
+##         doBTagging=True,       # run b-tagging
+##         jetCorrLabel='Scone5', # example jet correction name; set to None for no JEC
+##         doType1MET=True)       # recompute Type1 MET using these jets
+
+## add kt4 CaloJet collection
+addJetCollection(process, 'kt4CaloJets', 'KT4Calo', runCleaner="CaloJet",
+                 doJTA=True, doBTagging=True, jetCorrLabel='FKt4', doType1MET=True, doL1Counters=False)
+## add kt5 CaloJet collection
+addJetCollection(process, 'kt6CaloJets', 'KT6Calo', runCleaner="CaloJet",
+                 doJTA=True, doBTagging=False,jetCorrLabel='FKt6', doType1MET=True, doL1Counters=False)
+## add kt4 PflowJet collection
+addJetCollection(process,'sisCone5PFJets', 'SC5PFlow', runCleaner="PFJet",
+                 doJTA=True, doBTagging=True, jetCorrLabel='FKt4', doType1MET=True, doL1Counters=False)
+## add kt4 PflowJet collection
+addJetCollection(process,'kt4PFJets', 'KT4PFlow', runCleaner="PFJet",
+                 doJTA=True, doBTagging=True, jetCorrLabel='FKt4', doType1MET=True, doL1Counters=False)
+## add kt6 PflowJet collection
+addJetCollection(process,'kt6PFJets', 'KT6PFlow', runCleaner="PFJet",
+                 doJTA=True, doBTagging=True, jetCorrLabel='FKt6', doType1MET=True, doL1Counters=False)
 
 #-------------------------------------------------
 # process paths;
@@ -82,7 +109,8 @@ process.tqafEventContent = cms.PSet(
 ## define tqaf layer1 event content
 process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
 process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
-
+## process.tqafEventContent.outputCommands.extend(process.tqafLayer1EventContent_slim.outputCommands) ## drop genParticles
+process.tqafEventContent.outputCommands.extend(process.tqafLayer1EventContent.outputCommands) ## drop genParticles, add new jet collections
 
 #-------------------------------------------------
 # process output; first the event selection is
